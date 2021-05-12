ca8de60a324b46e8dee0e0f54635b3c9de4b6732
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-13 04:20:02 +0530

    added js logic

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log('app is running!')
+
+const screen = document.getElementById('screen');
+
+function display(val){
+    screen.value += val
+}
+
+function solve() {
+
+    let regex = /^([-+/*]\d+(\.\d+)?)*/
+    let str = screen.value
+
+    
+    try {
+        if (regex.test(str)) {
+            screen.value = eval(screen.value);
+        }
+    } catch (e) {
+        screen.value = 'Invalid Operation';
+        screen.classList.add('is-invalid')
+        console.log(e.message);
+    }
+}
+
+document.getElementById('delete').addEventListener('click', () => {
+    screen.classList.remove('is-invalid')
+    screen.value = ''
+})
