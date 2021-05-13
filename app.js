e85337ae47eec37288f6f14b9f311f3a99a9e305
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-13 11:25:13 +0530

    remove bug

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 console.log('app is running!')
 
-const screen = document.getElementById('screen');
+let screen = document.getElementById('screen')
 
 function display(val){
     screen.value += val
+    let regex = /^[\d\+\/\*\.\- \(\)]*$/
+    let str = screen.value
+    if (regex.test(str)) {
+        screen.classList.remove('is-invalid')
+    } else{
+        screen.value = 'Invalid Operation';
+        screen.classList.add('is-invalid')
+    }
 }
 
 function solve() {
-
-    let regex = /^([-+/*]\d+(\.\d+)?)*/
-    let str = screen.value
-
-    
     try {
-        if (regex.test(str)) {
-            screen.value = eval(screen.value);
-        }
+        screen.value = eval(screen.value)
     } catch (e) {
-        screen.value = 'Invalid Operation';
+        screen.value = 'Invalid Operation'
         screen.classList.add('is-invalid')
         console.log(e.message);
     }
